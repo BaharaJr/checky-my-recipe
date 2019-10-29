cac0bcbcada5124b8d32bbe8ec5d5e8efc25cf34
Bennett <bennybenester@gmail.com>
2019-10-29 17:04:06 +0300

    :sparkles: add shopping list components logic

--- src/app/components/shopping/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingridients } from '../../../shared/ingridients.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingridients:Ingridients[] = [
+    new Ingridients('Mangoes', 7),
+    new Ingridients('Organges', 10),
+    new Ingridients('Nyanya Chungu', 12),
+    new Ingridients('Malimau', 19),
+    new Ingridients('Bamia', 23),
+    
+  ];
 
   constructor() { }
 
