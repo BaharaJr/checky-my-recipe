3fad934d3c0cf2113e850847cfd3d038da7c6ac0
Bennett <bennybenester@gmail.com>
2019-10-29 12:13:02 +0300

    :sparkles: add the recipe component

--- src/app/components/recipe/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe/recipe-list/recipe-list.component.ts
@@ -1,8 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
-    selector: 'app-recipe-list',
-    templateUrl: './recipe-list.component.html',
-    styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
 })
-export class RecipeList {}
+export class RecipeList implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "Mambuta ya Bibi",
+      "Mambuta ya bibi tangu siku ya kwanza",
+      "assets/nick-bratanek-R0lEYv-K8os-unsplash.jpg"
+    )
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
